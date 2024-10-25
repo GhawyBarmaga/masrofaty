856b4ec24b409cd4e2d59b0b141efd76f4fae33c
mohanadaser <mohanadhassany4@gmail.com>
2024-10-25 14:46:02 +0200

    satrak

--- lib/main.dart
+++ lib/main.dart
@@ -21,9 +21,25 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+   @override
+  void initState() {
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        log('=========User is currently signed out!');
+      } else {
+        log('========User is signed in!');
+      }
+    });
+    super.initState();
+  }
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
